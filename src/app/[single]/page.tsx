a4466c0d207e600e32b81a2fe6b45e798c12bad4
brighter0611 <brighter0611@gmail.com>
2024-01-15 05:46:24 -0800

    first update

--- src/app/[single]/page.tsx
+++ src/app/[single]/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import { useParams } from "next/navigation";
 
 const Single = () => {
-    const params = useParams<{ tag: string; item: string }>()
+    const { single } = useParams();
     // console.log(params)
     return (
         <>
@@ -15,12 +15,12 @@ const Single = () => {
                 <div className='container-inner position-relative'>
                     <div className="title_section text-center">
                         <span className="title" style={{ color: '#222222' }}>
-                            <p>{params.single}</p>
+                            <p>{single}</p>
                         </span>
                     </div>
                     <div className="single-fruit text-center">
                         <Image
-                            src={`/images/${params.single}.jpg`}
+                            src={`/images/${single}.jpg`}
                             alt="fruit image"
                             width="800"
                             height="600"
